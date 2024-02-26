a976fff3d22f5e32a632717be7d9b05e97166bdf
Mathilde BESSON <mathilde.besson2023@campus-eni.fr>
2024-02-26 14:31:48 +0100

    nouvelle fonctionnalité

--- src/testgithub/App.java
+++ src/testgithub/App.java
@@ -3,7 +3,7 @@ package testgithub;
 public class App {
 
 	public static void main(String[] args) {
-		System.out.println("Message GitHub.");
+		System.out.println("Message GitHub." +Functions.mult(5, 2));
 	}
 
 }
---
+++ src/testgithub/Functions.java
@@ -0,0 +1,8 @@
+package testgithub;
+
+public class Functions {
+
+	public static float mult(float a, float b) {
+		return a*b;
+	}
+}
